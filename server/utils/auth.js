143af3f0c2b20903173b24014ac05ecb7310fd38
Ashley's PC <ashlynn4567@gmail.com>
2022-06-28 23:19:28 -0500

    created connection to database via mongoose, also created authentication via JWT

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,4 +1,41 @@
 // imports
+const jwt = require("jsonwebtoken");
+
+// set token secret and expiration date
+const secret = "mysecret";
+const expiration = "1h";
 
 // exports
-module.exports = auth;
+module.exports = {
+    // function for authenticated routes
+    auth: function ({ req }) {
+        // allows token to be sent via req.body, req.query or headers
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        // ["Bearer", "<tokenvalue>"]
+        if (req.headers.authorization) {
+            token = token.split(" ").pop().trim();
+        };
+
+        if (!token) {
+            return req;
+        };
+
+        try {
+            // decode and attach user data to request object
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch {
+            console.log("Invalid token.");
+        };
+
+        // return updated request body
+        return req;
+    }, 
+
+    signToken: function ({ username, email, id }) {
+        const payload = { username, email, _id };
+
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    }
+};
