c108fb096a79349f0265ebacb92b7dada6f7d484
Ashley's PC <ashlynn4567@gmail.com>
2022-07-05 21:59:41 -0500

    created typedef for character and updated typedef for users

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,6 +6,8 @@ const typeDefs = gql`
         _id: ID
         username: String
         email: String
+        savedCharacters: [Character]
+        characterCount: Int
     }
 
     type Character {
@@ -57,10 +59,198 @@ const typeDefs = gql`
                     }
                 }
                 armorClass: Int
-                ini
+                initiative: Int
+                speed: Int
+                hitPoints: [
+                    {
+                        maximum: Int
+                        current: Int
+                        temporary: Int
+                        hitDice: {
+                            total: String
+                            current: String
+                        }
+                    }
+                ]
+                skills: [
+                    acrobatics: {
+                        proficient: Boolean
+                        value: Int
+                    }
+                    animalHandling: {
+                        proficient: Boolean
+                        value: Int
+                    }
+                    arcana: {
+                        proficient: Boolean
+                        value: Int
+                    }
+                    athletics: {
+                        proficient: Boolean
+                        value: Int
+                    }
+                    deception: {
+                        proficient: Boolean
+                        value: Int
+                    }
+                    history: {
+                        proficient: Boolean
+                        value: Int
+                    }
+                    insight: {
+                        proficient: Boolean
+                        value: Int
+                    }
+                    intimidation: {
+                        proficient: Boolean
+                        value: Int
+                    }
+                    investigation: {
+                        proficient: Boolean
+                        value: Int
+                    }
+                    medicine: {
+                        proficient: Boolean
+                        value: Int
+                    }
+                    nature: {
+                        proficient: Boolean
+                        value: Int
+                    }
+                    perception: {
+                        proficient: Boolean
+                        value: Int
+                    }
+                    performance: {
+                        proficient: Boolean
+                        value: Int
+                    }
+                    persuasion: {
+                        proficient: Boolean
+                        value: Int
+                    }
+                    religion: {
+                        proficient: Boolean
+                        value: Int
+                    }
+                    sleightOfHand: {
+                        proficient: Boolean
+                        value: Int
+                    }
+                    stealth: {
+                        proficient: Boolean
+                        value: Int
+                    }
+                    survival: {
+                        proficient: Boolean
+                        value: Int
+                    }
+                ]
+                passivePerception: Int
             }
+            weapons: [
+                {
+                    weaponName: String
+                    weaponType: String
+                    proficient: Boolean
+                    attackBonus: Int
+                    damage: {
+                        damageType: String
+                        damageValue: String
+                    }
+                    properties: [String]
+                }
+            ]
+            proficiencies: {
+                tools: [String]
+                languages: [String]
+                armor: [String]
+                weapons: [String]
+            }
+            equipment: {
+                items: [
+                    {
+                        itemName: String
+                        itemType: String
+                        itemQuantity: Int
+                        itemWeight: Int
+                        itemRarity: String
+                        itemDescription: String
+                    }
+                ]
+                currency: {
+                    copperPieces: Int
+                    silverPieces: Int
+                    electrum: Int
+                    goldPieces: Int
+                    platinumPieces: Int
+                }
+            }
+            featuresAndTraits: [
+                {
+                    name: String
+                    origin: String
+                    description: String
+                }
+            ]
         }
-
+        backstoryAndPersonality: {
+            age: Int
+            height: String
+            weight: String
+            eyes: String
+            skin: String
+            hair: String
+            personalityTraits: [{String}]
+            ideals: [{String}]
+            bonds: [{String}]
+            flaws: [{String}]
+            backstory: String
+            alliesAndOrganizations: String
+            treasure: String
+        }
+        spells: {
+            spellcastingClass: String
+            spellcastingAbility: String
+            spellSaveDC: Int
+            spellAttackBonus: Int
+            spellSlotsTotal: {
+                level1: Int
+                level2: Int
+                level3: Int
+                level4: Int
+                level5: Int
+                level6: Int
+                level7: Int
+                level8: Int
+                level9: Int
+            }
+            spellSlotsExpended: {
+                level1: Int
+                level2: Int
+                level3: Int
+                level4: Int
+                level5: Int
+                level6: Int
+                level7: Int
+                level8: Int
+                level9: Int
+            }
+            spellList: [
+                {
+                    name: String
+                    level: Int
+                    type: String
+                    castingTime: String
+                    range: String
+                    target: String
+                    components: [{String}]
+                    duration: String
+                    prepared: Boolean
+                    description: String
+                }
+            ]
+        }
     }
 
     type Auth {
@@ -74,19 +264,5 @@ const typeDefs = gql`
         user(username: String!): User
     }
 `;
-
-//  TO-DO: type User needs the following:
-//  characterCount: Int
-//  savedCharacters: [Character]
-
-// const typeDefs = gql`
-//     type Query {
-//         _id: ID
-//         username: String
-//         email: String
-//         savedCharacters: Array?
-//         characterCount: Int
-//     }
-// `;
 
 module.exports = typeDefs;
